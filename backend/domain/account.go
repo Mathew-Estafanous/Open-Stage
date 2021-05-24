c4e774f73bed870facec53ef5b8bde659b599785
TwinsRock88 <mathewestafanous13@gmail.com>
2021-05-24 18:41:24 -0400

    Create domain models and database implementation for accounts.

---
+++ backend/domain/account.go
@@ -0,0 +1,20 @@
+package domain
+
+type Account struct {
+	Id       int
+	Name     string
+	Username string
+	Password string
+	Email    string
+}
+
+type AccountStore interface {
+	Create(acc *Account) error
+	GetByUsername(username string) (Account, error)
+	Delete(id int) error
+}
+
+type AccountService interface {
+	Create(acc *Account) error
+	Delete(id int) error
+}
---
+++ backend/infrastructure/postgres/account.go
@@ -0,0 +1,51 @@
+package postgres
+
+import (
+	"database/sql"
+	"github.com/Mathew-Estafanous/Open-Stage/domain"
+)
+
+type postgresAccountStore struct {
+	db *sql.DB
+}
+
+func NewAccountStore(db *sql.DB) domain.AccountStore {
+	return &postgresAccountStore{db}
+}
+
+func (p *postgresAccountStore) Create(acc *domain.Account) error {
+	r, err := p.db.Query("INSERT INTO accounts (name, username, password, email) VALUES ($1, $2, $3, $4)",
+		acc.Name, acc.Username, acc.Password, acc.Email)
+	if err != nil {
+		return err
+	}
+	r.Next()
+	err = r.Scan(&acc.Id)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (p *postgresAccountStore) GetByUsername(username string) (domain.Account, error) {
+	r, err := p.db.Query("SELECT id, name, username, password, email FROM accounts WHERE username = $1", username)
+	if err != nil {
+		return domain.Account{}, err
+	}
+
+	r.Next()
+	var account domain.Account
+	err = r.Scan(&account.Id, &account.Name, &account.Username, &account.Password, &account.Email)
+	if err != nil {
+		return domain.Account{}, err
+	}
+	return account, err
+}
+
+func (p *postgresAccountStore) Delete(id int) error {
+	_, err := p.db.Exec("DELETE FROM accounts WHERE id = $1", id)
+	if err != nil {
+		return err
+	}
+	return nil
+}
