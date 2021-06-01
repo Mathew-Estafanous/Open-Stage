cb8922b549de1a645d512130e04de8a2a716d498
TwinsRock88 <mathewestafanous13@gmail.com>
2021-05-31 21:01:40 -0400

    Create Auth middleware that validates access token.

---
+++ backend/middleware/auth.go
@@ -0,0 +1,56 @@
+package middleware
+
+import (
+	"encoding/json"
+	"github.com/dgrijalva/jwt-go"
+	"log"
+	"net/http"
+	"os"
+	"strings"
+	"time"
+)
+
+func Auth(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		auth := strings.Split(r.Header.Get("Authorization"), "Bearer ")
+		if len(auth) != 2 {
+			writeError(w, "Authorization header not formatted correctly.")
+			return
+		}
+
+		tk, err := jwt.Parse(auth[1], func(token *jwt.Token) (interface{}, error) {
+			return []byte(os.Getenv("SECRET_KEY")), nil
+		})
+		if err != nil {
+			writeError(w, "Invalid access token.")
+			return
+		}
+
+		if !tk.Valid {
+			writeError(w, "Invalid access token.")
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	})
+}
+
+type response struct {
+	Msg       string    `json:"message"`
+	Sts       int       `json:"status"`
+	TimeStamp time.Time `json:"timestamp"`
+}
+
+func writeError(w http.ResponseWriter, msg string) {
+	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+	respErr := response{
+		Msg:       msg,
+		Sts:       http.StatusUnauthorized,
+		TimeStamp: time.Now(),
+	}
+	w.WriteHeader(respErr.Sts)
+	err := json.NewEncoder(w).Encode(respErr)
+	if err != nil {
+		log.Print(err)
+	}
+}
