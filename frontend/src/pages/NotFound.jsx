6c1b814c63b6f7b0dbff09e7b14fac0e5f87bd77
Mathew-Estafanous <mathewestafanous13@gmail.com>
2023-10-10 18:29:11 -0400

    Add missing React import

--- frontend/src/pages/NotFound.jsx
+++ frontend/src/pages/NotFound.jsx
@@ -8,7 +8,7 @@ export const NotFound = () => {
     <div className='notFound'>
       <img
         className='logo' src='/Logo.png' alt='Logo'
-        onClick={e => history.push('/')}
+        onClick={_ => history.push('/')}
       />
       <h1>Not Found</h1>
       <h3>Sorry, We couldn't find the page you were looking for!</h3>
