62a5339f068b5235d362e517402c680ed26ed716
Mathew-Estafanous <mathewestafanous13@gmail.com>
2023-10-10 21:01:39 -0400

    fix CORS origin for heroku website

--- backend/middleware/cors.go
+++ backend/middleware/cors.go
@@ -8,7 +8,7 @@ import (
 // CORS creates a middleware that will handle CORS for the API.
 func CORS() mux.MiddlewareFunc {
 	headerOpts := handlers.AllowedHeaders([]string{"X-Requested-With", "Content-Type", "Authorization", "Set-Cookies"})
-	originOpts := handlers.AllowedOrigins([]string{"http://localhost:3000", "https://open-stage-web.herokuapp.com/"})
+	originOpts := handlers.AllowedOrigins([]string{"http://localhost:3000", "https://open-stage-web.herokuapp.com"})
 	methodOpts := handlers.AllowedMethods([]string{"GET", "POST", "PUT", "DELETE", "OPTIONS"})
 	credentialsOpts := handlers.AllowCredentials()
 	return handlers.CORS(headerOpts, originOpts, methodOpts, credentialsOpts)
